afafee4df459a567d7941a15230c1289d26c4802
Gui Matos <guilhermematos851@gmail.com>
2022-02-11 13:53:14 -0300

    Criado Classes / Atributos

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -1,59 +1,34 @@
-package com.api.parkingcontrol.models;
+package com.api.parkingcontrol.dtos;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+public class ParkingSpotDto {
 
-@Entity
-@Table(name = "TB_PARKING_SPOT")
-public class ParkingSpotModel implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID id;
-
-	@Column(nullable = false, unique = true, length = 10)
+	@NotBlank
 	private String parkingSpotNumber;
 
-	@Column(nullable = false, unique = true, length = 7)
+	@NotBlank
+	@Size(max = 7)
 	private String licensePlateCar;
 
-	@Column(nullable = false, length = 70)
+	@NotBlank
 	private String brandCar;
 
-	@Column(nullable = false, length = 70)
+	@NotBlank
 	private String modelCar;
 
-	@Column(nullable = false, length = 70)
+	@NotBlank
 	private String colorCar;
 
-	@Column(nullable = false)
-	private LocalDateTime registrationDate;
-
-	@Column(nullable = false, length = 130)
+	@NotBlank
 	private String reponsibleName;
 
-	@Column(nullable = false, length = 30)
+	@NotBlank
 	private String apartment;
 
-	@Column(nullable = false, length = 30)
+	@NotBlank
 	private String block;
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
 
 	public String getParkingSpotNumber() {
 		return parkingSpotNumber;
@@ -95,14 +70,6 @@ public class ParkingSpotModel implements Serializable {
 		this.colorCar = colorCar;
 	}
 
-	public LocalDateTime getRegistrationDate() {
-		return registrationDate;
-	}
-
-	public void setRegistrationDate(LocalDateTime registrationDate) {
-		this.registrationDate = registrationDate;
-	}
-
 	public String getReponsibleName() {
 		return reponsibleName;
 	}
@@ -125,10 +92,6 @@ public class ParkingSpotModel implements Serializable {
 
 	public void setBlock(String block) {
 		this.block = block;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 
 }
