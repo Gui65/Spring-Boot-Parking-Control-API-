0ea3f7155843f8649a817bd6748f8041750e9f8f
Gui Matos <guilhermematos851@gmail.com>
2022-02-11 17:46:59 -0300

    Criando métodos de cadastrar / Listar

--- src/main/java/com/api/parkingcontrol/Application.java
+++ src/main/java/com/api/parkingcontrol/Application.java
@@ -1,9 +1,14 @@
 package com.api.parkingcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parkingcontrol.controllers.ParkingSpotController;
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
 
 @SpringBootApplication
 @RestController
@@ -11,11 +16,28 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		
 	}
 	
+	@Autowired
+	ParkingSpotController parkingSpotController;
+	
 	@GetMapping("/")
-	public String index() {
-		return "Olá Mundo!";
+	public ResponseEntity<Object> index() {
+		ParkingSpotDto estacionamento = new ParkingSpotDto();
+		estacionamento.setApartment("XS");
+		estacionamento.setBlock("X");
+		estacionamento.setColorCar("X");
+		estacionamento.setBrandCar("X");
+		estacionamento.setLicensePlateCar("X");
+		estacionamento.setModelCar("X");
+		estacionamento.setParkingSpotNumber("X");
+		estacionamento.setReponsibleName("X");
+		
+		
+		return parkingSpotController.saveParkingSpot(estacionamento);
+		
 	}
 
 }
--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +33,23 @@ public class ParkingSpotController {
 	
 	@PostMapping
 	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+		if(parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License PLate Car is already in use");
+		}
+		if(parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use");
+		}
+		if(parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered fot this apartment/block");
+		}
 		var parkingSpotModel = new ParkingSpotModel();
 		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
-		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTF")));
+		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots(){
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+		}
 }
